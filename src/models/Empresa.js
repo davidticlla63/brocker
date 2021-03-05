d5aa5ad7e1df4eec0f8e994d59ca1efd76c34c8e
David Noe Ticlla Felipe <dticlla@gmsantacruz.gob.bo>
2021-03-05 11:12:45 -0400

    empresa

---
+++ src/models/Empresa.js
@@ -0,0 +1,28 @@
+import Sequelize from "sequelize";
+import { sequelize } from "../database/database";
+import Sucursal from "./Sucursal";
+
+const Empresa = sequelize.define('empresa', {
+    id: {
+        //defaultValue: Sequelize.literal('uuid_generate_v4()'),
+        type: Sequelize.BIGINT,
+        primaryKey: true    
+    },
+    razonsocial:Sequelize.STRING,
+    descripcion:Sequelize.STRING,
+    telefono:Sequelize.STRING,
+    nit:Sequelize.STRING,
+    representante:Sequelize.STRING,
+    logo:Sequelize.BLOB,
+    fecharegistro:Sequelize.DATE(6),
+    fechamodificacion:Sequelize.DATE(6),
+    estado:Sequelize.STRING,
+    
+}, {
+    timestamps: false
+});
+
+
+Project.hasMany(Sucursal, { foreignKey: 'empresaid', sourceKey: 'id' });
+Task.belongsTo(Empresa, { foreignKey: 'empresaid', sourceKey: 'id' });
+export default Empresa;
