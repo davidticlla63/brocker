d7164c4b87162d0696746fbccad3d670b9af6a04
David Noe Ticlla Felipe <dticlla@gmsantacruz.gob.bo>
2021-03-10 16:05:41 -0400

    proyecto modificado

--- src/controllers/empresa.controller.js
+++ src/controllers/empresa.controller.js
@@ -5,8 +5,8 @@ import Sucursal from "../models/Sucursal";
 
 export async function getEmpresas(req, res) {
     try {
-        
-        const empresas = await Empresa.findAll({include:Sucursal});
+       // const empresas = await Empresa.findAll({estado:'ACT', include:Sucursal});
+        const empresas = await Empresa.findAll({where :{estado:'ACT'},include:Sucursal});
         res.json({
             data: empresas
         });
