148befc6adf19ea1392237b1134fade1ffeec464
davidticlla63 <david.ticlla@gmail.com>
2022-07-25 21:24:26 -0400

    cambios

--- src/controllers/memo.controller.js
+++ src/controllers/memo.controller.js
@@ -701,10 +701,11 @@ export async function getTotalProduccionMemoPorSucursal(req, res) {
             let query = ` with consulta as(select count(*) cantidad,( case when p.ingresoegreso ='I' then SUM(p.primaneta) else 0 end -case when p.ingresoegreso ='E' then SUM(p.primaneta) else 0 end)  totalvalorasegurado 
             from memo m  
             inner join poliza p on p.id=m.polizaid and p.estado in ('ACT','CER')  
+            inner join sucursal s on s.id =p.sucursalid
             where m.estado  in ('ACT') and 
             extract(year from  m.fechamemo)=  extract(year from  now())-- and extract(month  from   m.fechamemo)=  (select MAX(m2 .mesproduccion) from memo m2 where m2.sucursalid=m.sucursalid) 
             and m.mesproduccion = case when extract(DAY from  now())>(select pp.diaproduccion from param_produccion pp where pp.sucursalid=s.id) then extract(month from  now()) else extract(month from  now())-1 end
-             and m.sucursalid ='` + sucursalid + `'
+             and s.id ='` + sucursalid + `'
             group by p.ingresoegreso 
             )
             
